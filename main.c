0accfe5b836a327b86e5a8ce7e79a2c2ca2ba190
Arif De Sousa <arifads@gmail.com>
2013-02-13 20:20:39 -0430

    Here We Go

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  MCDS
+//
+//  Created by Arif De Sousa on 2/13/13.
+//  Copyright (c) 2013 Arif De Sousa. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(){
+    printf("hola");
+    return 0;
+}
